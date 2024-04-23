131ac96ba2363a9aa901b2d8db911fc9a87ce404
WeijianXu <weijian.xu@unidt.com>
2024-04-23 16:21:42 +0800

    fix: 去掉不必要的eachRound调用

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: WeijianXu weijian.xu@unidt.com
  * @Date: 2024-04-17 15:15:45
  * @LastEditors: WeijianXu weijian.xu@unidt.com
- * @LastEditTime: 2024-04-23 15:43:28
+ * @LastEditTime: 2024-04-23 16:20:20
  * @FilePath: \output-verbatim\src\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -108,7 +108,7 @@ export default class VerbatimOutput {
     if (options.start && options.start >= 0) {
       preText = text.substring(0, options.start);
       richText = text.substring(options.start);
-      options.eachRound && options.eachRound(preText, '');
+      // options.eachRound && options.eachRound(preText, '');
     }
 
     if (!richText) {
