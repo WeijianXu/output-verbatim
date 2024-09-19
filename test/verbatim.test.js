155d74e975db19f535744b637261cfaf2fcb4ff0
WeijianXu <weijian.xu@unidt.com>
2024-09-19 10:41:21 +0800

    docs: 修正单词

--- test/verbatim.test.js
+++ test/verbatim.test.js
@@ -2,7 +2,7 @@
  * @Author: WeijianXu weijian.xu@unidt.com
  * @Date: 2024-04-22 17:45:54
  * @LastEditors: WeijianXu weijian.xu@unidt.com
- * @LastEditTime: 2024-04-25 17:24:24
+ * @LastEditTime: 2024-07-22 16:37:04
  * @FilePath: \output-verbatim\test.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,7 +31,7 @@ describe('Checking output verbatim', () => {
     });
   });
 
-  it('should transfer markdow content correctly', (done) => {
+  it('should transfer markdown content correctly', (done) => {
     let round = 0;
     const output = new Verbatim('# Output Verbatim\n<b>H</b>ello, <b>W</b>orld!  <b>T</b>he <b>I</b>s <b>O</b>utput <b>V</b>erbatim.', {
       speed: 30, // Printing speed per word, 30 milliseconds a word, a cycle
